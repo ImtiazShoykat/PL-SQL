5a137dbcb38dfa53c1dae399009c50effd8adf55
ImtiazShoykat <imtiazshoykat@gmail.com>
2024-02-11 13:04:28 +0600

    long query

--- test/emp_Test.sql
+++ test/emp_Test.sql
@@ -1,1 +1,12 @@
-select * from employees
+/* Formatted on 2/11/2024 12:54:31 PM (QP5 v5.396) */
+/* Filtering data without where clause */
+
+SELECT department_name,
+       last_name,
+       salary,
+       d.department_id,
+       e.department_id
+  FROM employees  e
+       JOIN departments d
+           ON     (e.department_id = d.department_id)
+              AND d.department_id IN (90, 60)
